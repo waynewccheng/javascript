bc9128b2f1bbb49418cf18bfd6a237cac6f56937
waynewccheng <45103074+waynewccheng@users.noreply.github.com>
2020-05-09 13:13:02 -0400

    Original

---
+++ toilet.js
@@ -0,0 +1,148 @@
+/***
+Thanks to programing king for letting me use his anti plagiarizing function(I edited it a lot and u can try to defeat it)
+https://stackoverflow.com/questions/16873323/javascript-sleep-wait-before-continuing
+**/
+var HAI = "HEY TRYING TO PLAGERIZE?????";
+var CREDIT = "Proraming King";
+var CREATOR = "Renren2020";
+var CREATOR_ALPHABETICALhahahurstillonfirethisoutDECODE = [44,5,14,18,5,14];
+var dudeuridkCREATORblue_teeheezLETTERwhosethereDECODE=[101100,101,1110,10010,101,1110];
+var CREATERho=function(idk){
+    if(idk!==CREATOR){}
+};
+var acualTEEHEEBOOOOOOHAIILOOOnumsforCREATOR=[10,0,10,0];
+var igivewhatcreditCREDITistoDECODEmuhhahahDECODE=[101010,10010,1111,111,10010,1,1101,1001,,111];
+var circle=function(x,y,d){
+    ellipse(x,y,d,d);
+};
+var ad=5; //1=~29-30 2= 5=5 10=4
+var scene1=function(){
+background(135, 128, 128);
+noStroke();
+fill(255, 255, 255);
+
+var rext=function(){
+    var CREDITANDCREATORRDESAME=0;
+    if(CREATOR!=="Renren 2020"&&CREATOR!=="Renren2020"){
+        throw{message:"HEY!!!"};
+    }
+};
+rext();//if u delete me u can plagerize YAY
+var tiltedrectangle=function(x,y){
+for(var i=0;i<120;i++){
+    ellipse(x-20+i,y-20+i,40,40);
+}
+};
+tiltedrectangle(1,91);
+rect(100,170,200,40);
+rect(300,170,40,149);//this hides the two corners for the rectangle
+rect(300,170,40,169,30);
+rotate(30);
+rect(260,161,40,90);
+rect(418,115,40,66);
+rotate(-60);
+fill(255, 255, 255);
+rect(11,340,40,113);
+rotate(30);
+rect(238,348,70,40);
+rect(143,269,70,40);
+rect(64,328,70,40);
+rect(24,328,40,200);
+rect(64,488,244,40);
+rext(64,202,40,200);
+rotate(45);
+rect(552,-117,40,271);
+rect(462,-385,40,193);
+rotate(-45);
+rect(462,193,40,143);
+};
+scene1();
+fill(0,0,0);
+var thingy=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+draw= function() {
+    var réct=function(){};
+    scene1();
+    fill(255, 0, 255);
+    réct();
+    
+    
+    //var makeenemyappear=function(secs_apart){
+    //var mills_apart=secs_apart/1000;
+    //if(millis()%mills_apart===0){
+    if(thingy[0]<110){
+        ellipse(-10+thingy[0],82+thingy[0],20,20);
+        thingy[0]=thingy[0]+ad;
+    }
+    else
+        if(thingy[1]<220){
+            ellipse(100+thingy[1],192,20,20);
+            thingy[1]=thingy[1]+ad;
+    }
+    else
+        if(thingy[2]<135){
+            ellipse(320,192+thingy[2],20,20);
+            thingy[2]=thingy[2]+ad;
+        }
+    else
+        if(thingy[3]<40){
+            ellipse(320-(thingy[3]*(1/1.5)),192+thingy[2]+(thingy[3]),20,20);
+            thingy[3]=thingy[3]+ad;
+        }
+    else
+        if(thingy[4]<35){
+            ellipse(293.3-thingy[4],367,20,20);
+            thingy[4]=thingy[4]+ad;
+        }
+    else
+        if(thingy[5]<75){
+            circle(258-(thingy[5]*(1/1.5)),367-thingy[5],20);
+            thingy[5]=thingy[5]+ad;
+        }
+    else
+        if(thingy[6]<50){
+            circle(205-thingy[6],292,20);
+            thingy[6]=thingy[6]+ad;
+        }
+    else
+        if(thingy[7]<55){
+            circle(155-(thingy[7]*(1/1.5)),292+thingy[7],20);
+            thingy[7]=thingy[7]+ad;
+        }
+    else
+        if(thingy[8]<80){
+            ellipse(118.3-thingy[8],342,20,20);
+            thingy[8]=thingy[8]+ad;
+        }
+    else
+        if(thingy[9]<165){
+            ellipse(41.6,342+thingy[9],20,20);
+            thingy[9]=thingy[9]+ad;
+    }    
+    else
+        if(thingy[10]<265){
+            ellipse(41.6+thingy[10],507,20,20);
+            thingy[10]=thingy[10]+ad;
+        }
+    else
+        if(thingy[11]<180){
+            ellipse(306.6+thingy[11],507-thingy[11],20,20);
+            thingy[11]=thingy[11]+ad;
+        }    
+    else
+        if(thingy[12]<110){
+            ellipse(486.6,327-thingy[12],20,20);
+            thingy[12]=thingy[12]+ad;
+        }
+    else
+        if(thingy[13]<130){
+            ellipse(485+thingy[13],207-thingy[13],20,20);
+            thingy[13]=thingy[13]+ad;
+        }
+    //}    
+    //};
+    //makeenemyappear(2);
+    /**else
+        if(thingy[13]>1){
+            thingy=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        }**/
+};  
